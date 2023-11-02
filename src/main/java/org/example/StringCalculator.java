10de1ada6af0eb1646e45e7c514d95fa80de3b39
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2023-11-02 18:44:34 +0100

    [feat]: Handle negative number

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,32 +1,48 @@
 package org.example;
 
-import static java.lang.Integer.parseInt;
+import java.util.List;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 
 public class StringCalculator {
 
-    public int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
-
-        String[] numsArray = retrieveNumbers(numbers);
+    public int add(String numbers) throws Exception {
+        String regex = getRegex(numbers);
+        List<Integer> numsArray = getNumbers(numbers, regex);
 
         int result = 0;
-        for (String number : numsArray) {
-            result += parseInt(number);
+        for (int number : numsArray) {
+            if (isNegativeNumber(number)) {
+                throw new Exception("negatives not allowed: " + number);
+            }
+            result += number;
         }
         return result;
     }
 
-    private static String[] retrieveNumbers(String numbers) {
-        String regex = ",|\\n";
+    private String getRegex(String numbers) {
+        return (hasDynamicDelimiter(numbers))
+                ? numbers.substring(2, numbers.indexOf("\n"))
+                : ",|\\n";
+    }
 
-        if (numbers.startsWith("//")) {
-            int endDelimiters = numbers.indexOf("\n");
-            regex = numbers.substring(2, endDelimiters);
-            numbers = numbers.substring(endDelimiters + 1);
+    private List<Integer> getNumbers(String numbers, String regex) {
+        if (hasDynamicDelimiter(numbers)) {
+            numbers = numbers.substring(numbers.indexOf("\n") + 1);
         }
 
-        return numbers.split(regex);
+        return stream(numbers.split(regex))
+                .filter(it -> !it.isEmpty())
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+
+    private boolean hasDynamicDelimiter(String numbers) {
+        return numbers.startsWith("//");
+    }
+
+    private boolean isNegativeNumber(int number) {
+        return number < 0;
     }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -3,57 +3,64 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCalculatorTest {
 
     private final StringCalculator stringCalculator = new StringCalculator();
 
     @Test
-    void zero_value_when_the_string_value_is_empty() {
+    void zero_value_when_the_string_value_is_empty() throws Exception {
         int result = stringCalculator.add("");
 
         assertEquals(0, result);
     }
 
     @Test
-    void sum_value_when_the_string_has_single_value() {
+    void sum_value_when_the_string_has_single_value() throws Exception {
         int result = stringCalculator.add("1");
 
         assertEquals(1, result);
     }
 
     @Test
-    void sum_value_when_the_string_has_two_values() {
+    void sum_value_when_the_string_has_two_values() throws Exception {
         int result = stringCalculator.add("1,2");
 
         assertEquals(3, result);
     }
 
     @Test
-    void sum_value_when_the_string_has_more_values() {
+    void sum_value_when_the_string_has_more_values() throws Exception {
         int result = stringCalculator.add("1,2,3");
 
         assertEquals(6, result);
     }
 
     @Test
-    void handle_new_line_delimiter_with_two_values() {
+    void handle_new_line_delimiter_with_two_values() throws Exception {
         int result = stringCalculator.add("1\n2");
 
         assertEquals(3, result);
     }
 
     @Test
-    void handle_new_line_delimiter() {
+    void handle_new_line_delimiter() throws Exception {
         int result = stringCalculator.add("1\n2,3");
 
         assertEquals(6, result);
     }
 
     @Test
-    void handle_dynamic_delimiters() {
+    void handle_dynamic_delimiters() throws Exception {
         int result = stringCalculator.add("//;\n1;2");
 
         assertEquals(3, result);
+    }
+
+    @Test
+    void throw_exception_with_negative_numbers() {
+        Exception exception = assertThrows(Exception.class, () -> stringCalculator.add("1,4,-1"));
+        assertEquals("negatives not allowed: -1", exception.getMessage());
     }
 }
