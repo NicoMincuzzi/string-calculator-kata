86df67cf51c9ddff417055884c6a746d35a07cd8
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2023-11-02 09:10:39 +0100

    [feat]: Handle empti string as a value

---
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringCalculatorTest {
+
+    private final StringCalculator stringCalculator = new StringCalculator();
+
+    @Test
+    void
+    zero_value_when_the_string_value_is_empty() {
+        int result = stringCalculator.add("");
+
+        assertEquals(0, result);
+    }
+}
